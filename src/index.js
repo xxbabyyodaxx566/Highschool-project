9bb16ecacceba579ab356ba5e6b133862ecd0a5d
xxbabyyodaxx566 <79723153+xxbabyyodaxx566@users.noreply.github.com>
2021-08-24 12:02:36 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+document.getElementById("app").innerHTML = `
+<h1>Hello World!</h1>
+<div>
+  this is a web page im starting on my second day of highschool the coding languages include json html5 javascript and css
+</div>
+`;
+//snowden
